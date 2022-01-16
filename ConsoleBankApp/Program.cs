db30e3e4dd7a4761f3205cba8c59efaf9039564f
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-01-16 12:07:44 +0300

    ДЗ 5

--- ConsoleBankApp/Program.cs
+++ ConsoleBankApp/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using GB_U_OOP;
-using GB_U_OOP.BuilderPattern;
-
 
 namespace ConsoleBankApp
 {
@@ -11,30 +7,15 @@ namespace ConsoleBankApp
     {
         static void Main(string[] args)
         {
-            BuilderController controller = new BuilderController();
-            BuildingBuilder builder = new BuildingBuilder();
-            BuildingBuilder builder_2 = new BuildingBuilder();
-            controller.Builder = builder;
-            controller.CreateTypeA();
-            PrintInfo(builder.GetBuilding());
+            RationalNumber a = new RationalNumber(3, 5);
+            RationalNumber b = new RationalNumber(2, 15);
 
-            Console.WriteLine("==================");
-            controller.Builder = builder_2;
-            controller.CreateTypeB();
-            PrintInfo(builder_2.GetBuilding());
+           // RationalNumber s = a + b;
+            a++;
+            Console.WriteLine(a.ToString());
+
             Console.ReadKey();
         }
-
-        private static void PrintInfo(Building building)
-        {
-            Console.WriteLine($"Building: {building.Name}; ID: {building.Id};" );
-            Console.WriteLine($"\t-Section count: {building.SectionCount};" );
-            Console.WriteLine($"\t-Max height: {building.MaxHeight};" );
-            Console.WriteLine($"\t-Max level count: {building.MaxLevelCount};" );
-            Console.WriteLine($"\t-Apartment Count: {building.ApartmentCount};" );
-        }
-       
     }
 
-   
 }
